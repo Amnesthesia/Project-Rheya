9a6db5077960e9d6b1029a0f8d22605706d4d109
Amnesthesia <sintpanda@gmail.com>
2013-05-11 22:20:29 +0200

    Added sentence

---
+++ sentence.rb
@@ -0,0 +1,48 @@
+require "treat"
+include Treat::Core::DSL
+
+class Sentence
+  attr_accessor :who, :to_whom, :is_question, :is_exclamation, :mood, :target, :nouns, :verbs, :adjectives, :adverbs, :words
+  
+  def initialize(*args)
+    @sentence = args[0]
+    @nouns = []
+    @adjectives = []
+    @words = []
+    @verbs = []
+    @adverbs = []
+    @to_whom = []
+    
+    analyze(@sentence)
+  end
+  
+  def analyze(msg)
+    t = msg.to_entity.do(:chunk,:segment, :tokenize, :category)
+
+    t.each_word do |m|
+      unless m.to_s == nil
+        @words << m.to_s
+        if m.category == "noun"
+          @nouns << [m.to_s.singular,t.frequency_of(m.to_s)] unless @nouns.include? m
+        elsif m.category == "verb" and !@verbs.include? m.to_s.stem
+          @verbs << m.to_s.stem
+        elsif m.category == "adjective" and !@adjectives.include? m.to_s
+          @adjectives << m.to_s
+        elsif m.category == "adverb" and !@adverbs.include? m.to_s
+          @adverbs << m.to_s
+        elsif m.category == "interrogation"
+          @is_question = true
+        elsif m.category == "exclamation"
+          @is_exclamation = true
+        elsif m.to_s != "I" and m.category == "pronoun"
+          @to_whom << m.to_s
+        elsif m.to_s == "I" and m.category == "pronoun"
+          @who = "source"
+        end
+      end
+    end
+  end
+  
+  
+  
+end
--- think.rb
+++ think.rb
@@ -1,5 +1,38 @@
+require "treat"
 
-
-class brain
+class Brain
+  
+  def initialize(*args)
+  end
+  
+  
+  def get_topic(msg)
+    score = 0
+    
+    nouns = get_nouns(msg)
+    
+    topic = []
+    nouns.each do |n|
+      if n[1].to_i > score
+        topic << n[0]
+        score = n[1].to_i
+      end
+    end
+    
+    return topic
+  end
+  
+  def get_nouns(msg)
+    s = sentence(msg)
+    nouns = []
+    s.each do |m|
+      nouns << m[1]
+    end
+    return nouns
+  end
+  
+  def get_adjectives
+  end
+  
   
 end

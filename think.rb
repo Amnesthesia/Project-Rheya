a798dbb0d34fbd146f2b71bd010e8838a754f947
Amnesthesia <sintpanda@gmail.com>
2013-05-28 23:27:02 +0200

    Updated brain

--- think.rb
+++ think.rb
@@ -11,23 +11,26 @@ class Brain
     score = 0
     
     nouns = get_nouns(msg)
+
+    # Sort nouns by value (which is their occurance)
+    nouns.sort_by{|k,v| v}.reverse
     
-    topic = []
-    nouns.each do |n|
-      if n[1].to_i > score
-        topic << n[0]
-        score = n[1].to_i
-      end
+    if nouns.count > 3
+      maximum_topics = (nouns.count / 2).round
+      nouns = nouns[0...maximum_topics]
     end
     
-    return topic
+    
+    return nouns
   end
   
   def get_nouns(msg)
-    s = sentence(:export_name,msg)
-    nouns = []
-    s.each do |m|
-      nouns << m[1]
+    s = Sentence.new(:export_name,msg)
+    
+    nouns = {}
+   
+    s.nouns.each do |m|
+      nouns[m[0].to_sym] = m[1]
     end
     return nouns
   end
